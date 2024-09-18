8b318bb3feb31ee6befe3bd490d59d0c45908f54
DwikiAprilianAryanda <aprilianaryandad@gmail.com>
2024-09-18 19:44:55 +0800

    Pertemuan 3

---
+++ Kelas APD A'24/Pertemuan-3/Pertemuan 3.py
@@ -0,0 +1,53 @@
+# umur = 6
+# if umur <5 and umur >0:
+#     print("Umur lebih dari 5")
+# elif umur <10 and umur >5:
+#     print("Umur lebih dari 5")
+# else:
+#     print("Umur lebih dari 10")
+
+# cuaca = str(input("Masukkan cuaca= "))
+# if cuaca == "cerah":
+#     print("Pergi ke sekolah")
+# elif cuaca == "hujan":
+#     print('Tetap di rumah')
+# else:
+#     print("Invalid")
+
+# x = 2
+# match x:
+#     case 0:
+#         print("False")
+#     case 1:
+#         print("True")
+
+# x = 32
+# if x>= 10:
+#     print("Lebih dari sama dengan 10")
+#     if x > 20:
+#         print("Lebih dari 20 juga")
+#         if x > 30:
+#             print("Angkanya lebih dari 30 juga")
+# else:
+#     print("Kurang dari 10")
+
+# angka = int(input("Masukkan angka "))
+
+# if angka >30:
+#     print("sapi")
+#     if angka >50:
+#         print("singa")
+# else:
+#     print("Invalid")
+
+x = int(input("masukkan angka pertama= "))
+y = int(input("masukkan angka kedua= "))
+
+if x >y:
+    print("Kelinci")
+elif x <y:
+    print("Kura-kura")
+elif x ==y:
+    print("Kerbau")
+else:
+    print("Invalid")
