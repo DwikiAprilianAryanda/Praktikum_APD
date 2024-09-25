1aab8d77bc4669ef00f0245398505937d3aee610
DwikiAprilianAryanda <aprilianaryandad@gmail.com>
2024-09-25 09:07:07 +0800

    Pertemuan 4

---
+++ Kelas APD A'24/Pertemuan-4/Pertemuan 4.py
@@ -0,0 +1,79 @@
+# ulang = 10
+# for i in range(ulang):
+#     print(f'perulangan ke-{str(i)}')
+
+
+# simpan = [12 , 'udin petot' , 14.5 , true , 'A']
+# for i in simpan:
+#     print(i)
+
+    
+#     simpan = [12 , 'udin petot' , 14.5 , true , 'A']
+# for i in simpan:
+#     print(i, end=' ')
+
+
+# for i in range(1, 10, 2): #(awal, akhir, lompatan)
+#     print(i)
+
+# for i in range(1, 4):
+#     for j in range(1, 4):
+#         print(f'{i} x {j} = {i*j}')
+#     print()
+
+
+# jawab = 'ya'
+# hitung = 0
+
+# while(jawab == 'ya'): #kondisi atau syarat
+#     hitung +=1
+#     jawab = input('ulang lagi tidak? : ')
+# print(f'total perulangan : {hitung}')
+
+
+# jawab = 'ya'
+# hitung = 0
+
+# while True: #selalu benar
+#     hitung +=1
+#     jawab = input('ulang lagi tidak? : ')
+#     if jawab == 'tidak' or jawab == 'tidak': #kondisi berhenti
+#         break
+    
+# print(f'total perulangan : {hitung}')
+
+
+# print('Daftar bilangan ganjil dari 1-10')
+# for i in range(10):
+#     if i % 3 == 0:
+#         continue
+#     print(i)
+
+
+# jawab = int
+# angka = 0
+# while True:
+#     jawab = int(input('masukkan angka: '))
+#     if jawab <=0:
+#         break
+#     angka += jawab
+# print(f'angka nya adalah : {angka}')
+
+
+# for i in range(1, 20, 3):
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+
+# buah = ['apel' , 'jeruk' , 'semangka' , 'melon' , 'pisang']
+# buah_baru = []
+
+# for i in buah:        #artinya variabel buah ada di dalam i
+#     if 'e' in i:      #jika di dalam variabel i ada huruf e, maka print
+#         buah_baru.append(i)
+        
+# print(buah_baru)
+
+# # buah_baru = [i for i in buah if 'e' in i] 
+
+
